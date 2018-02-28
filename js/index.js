31ff931e33bb4cc1256597aedc7f2d3e0c47aaf7
Marta Kovaliv <marta.kovaliv@gmail.com>
2018-02-28 17:50:50 +0000

    FinalChange

--- js/index.js
+++ js/index.js
@@ -3,14 +3,14 @@ const b = parseFloat(prompt('Write B equivalent'));
 const c = parseFloat(prompt('Write C equivalent'));
 
 const result = quadraticEquation(a, b, c);
-    document.write (result);
+document.write(result);
 
 function quadraticEquation(a, b, c) {
     const d = countDiscr(a, b, c);
     if (d > 0) {
         const x1 = (-b + Math.sqrt(d)) / (2 * a);
         const x2 = (-b - Math.sqrt(d)) / (2 * a);
-        return = [x1; x2] ;
+        return [x1,x2];
     }
     else if (d === 0) {
         const x = -b / (2 * a);
